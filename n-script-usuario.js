11a79c4be4f38136705157cddd8cb057938c8bd0
CarlosCamuzzi <carloscamuzzi.hs@gmail.com>
2021-12-05 19:34:05 -0300

    att

---
+++ n-script-usuario.js
@@ -0,0 +1,39 @@
+// function carregarUsuario() {
+//     alert("entrou")
+//     location.load(dadosUser());   
+// }
+
+// function dadosUser() {
+//     //alert("entrou")
+//     let dadosUsuario = document.querySelector('#dados-usuario');
+//     listaUsuario = JSON.parse(localStorage.getItem('listaUsuario') || '[]');
+//     salvaCod = JSON.parse(localStorage.getItem('salvaCod') || '[]');
+
+//     for (let i = 0; i < localStorage.length; i++) {
+//         // alert(localStorage.length)
+//     }
+
+//     //  if (usuarioLogado == listaUsuario.codCad)
+    
+//     let key = localStorage.key(1);      // Método 'key'
+//     let saida = localStorage.getItem(key);
+
+//     dadosUsuario.insertAdjacentHTML("beforeend", `${usuarioLogado}<br>`,);
+//      dadosUsuario.insertAdjacentHTML("beforeend", saida);
+//     // dadosUsuario.insertAdjacentHTML("beforeend", listaUsuario[1].emailCad);
+//     // dadosUsuario.insertAdjacentHTML("beforeend", listaUsuario.senha);
+//     // dadosUsuario.insertAdjacentHTML("beforeend", listaUsuario.confsenha);
+//     // dadosUsuario.insertAdjacentHTML("beforeend", listaUsuario.codPessoa);
+// }
+
+// "emailCad": email.value,
+//                     "nomeCad": nome.value,
+//                     "cnpjCad": cnpj.value,
+//                     "senhaCad": senha.value,
+//                     "cadconfsenhaCad": confsenha.value,
+//                     "codCad": codPessoa
+
+// let display_valor = document.getElementById('display-valor')
+
+//     display_numero_nf.insertAdjacentHTML("beforeend", `${relatorio.dados[i].numeroNF} <br><br>`);
+
--- script-procurar.js
+++ script-procurar.js
@@ -1,77 +1,73 @@
-// function procurar() {
-
-//     //let razao_social = document.getElementById('razao-social')
-//     let display_razao_social = document.getElementById('display-razao-social')
-//     let numero_nf = document.getElementById('display-numero-nf')
-//     let data_emissao = document.getElementById('display-data-emissao')
-//     let chave_acesso = document.getElementById('display-chave-acesso')
-//     let valor = document.getElementById('display-valor')
-
-
-//     display_razao_social.innerHTML = display_razao_social.value
-//     numero_nf.innerHTML = '123'
-//     data_emissao.innerHTML = '05/11/2021'
-//     chave_acesso.innerHTML = '1234567891234567891234567891234567891234567'
-//     valor.innerHTML = "R$ 1250,00"
-// }
-
+// Getando relatório via array
 function procurar() {
+
     let relatorio = {
         dados: [
             {
-                numeroNF: '1234',
+                numeroNF: '12',
                 dataEmissao: '01/09/2021',
                 razaoSocial: 'Amazing LTDA',
                 chaveAcesso: '12345678912345678912345678912345678912345678',
-                valorNF: 'R$ 200,00'
+                valorNF: 'R$ 200.00'
             },
             {
                 numeroNF: '546',
-                dataEmissao: '01/10/2021',  
+                dataEmissao: '01/10/2021',
                 razaoSocial: 'ByeBye LTDA',
                 chaveAcesso: '12345678912345678912345678912345678912345678',
-                valorNF: 'R$ 10000,00'
+                valorNF: 'R$ 10000.00'
             },
             {
                 numeroNF: '546',
                 dataEmissao: '01/09/2021',
                 razaoSocial: 'Saudades LTDA',
                 chaveAcesso: '12345678912345678912345678912345678912345678',
-                valorNF: 'R$ 2200,00'
+                valorNF: 'R$ 2200.00'
             },
             {
                 numeroNF: '546',
                 dataEmissao: '01/10/2021',
-                razaoSocial: 'Abacate LTDA',
+                razaoSocial: 'Abacate',
                 chaveAcesso: '12345678912345678912345678912345678912345678',
-                valorNF: 'R$ 100,00'
+                valorNF: 'R$ 100.00'
             }
         ]
     }
     buscar(relatorio);
 }
 
+// Fazendo a busca de acordo com os dados informados pelo usuário
 function buscar(relatorio) {
 
-    let inputBuscar = document.querySelector('#nf');
-    let busca = Number(inputBuscar.value);
+    let inputNF = document.querySelector('#nf');
+    let inputRazaoSocial = document.querySelector('#razao-social'); // aqui
 
+    // Pegando valores da input do usuário
+    let buscaNum = Number(inputNF.value);
+    let buscaRazaoSocial = inputRazaoSocial.value;
+
+    // Iterando o array e verificando se a entrada corresponde com algum elemento
     for (let i = 0; i < relatorio.dados.length; i++) {
-        if (busca == relatorio.dados[i].numeroNF) {
-            exibirRelatorio(relatorio, i)
-            
-            // teste
-            // let validaImprimir = imprimir(x);
-            // if (validaImprimir == 1){
-            //     imprimir(relatorio,i);
-            // } else {
-            //     exibirRelatorio(relatorio, i);
-            // }
-            // teste
+        if (buscaNum == 0 && buscaRazaoSocial == false) {
+            //alert("entrou")
+            exibirRelatorio(relatorio, i);
+        }
+        if (buscaNum) {                 // Por número nota
+            if (buscaNum == relatorio.dados[i].numeroNF) {
+                exibirRelatorio(relatorio, i)
+            }
+        }
+        else if (buscaRazaoSocial) {        // Por razão social - string
+            // toUpperCase() para converter tudo para maiúscula, assim o usuário pode digitar em minúscula que não fará diferença
+            let strUpper = relatorio.dados[i].razaoSocial.toUpperCase();
+            if (buscaRazaoSocial.toUpperCase() == strUpper) {
+                exibirRelatorio(relatorio, i)
+            }
         }
     }
 }
 
+// Exibindo dados selecionados na tela
 function exibirRelatorio(relatorio, i) {
 
     let display_numero_nf = document.getElementById('display-numero-nf')
@@ -86,21 +82,141 @@ function exibirRelatorio(relatorio, i) {
     display_chave_acesso.insertAdjacentHTML("beforeend", `${relatorio.dados[i].chaveAcesso} <br><br>`);
     display_valor.insertAdjacentHTML("beforeend", `${relatorio.dados[i].valorNF} <br><br>`);
 
-    //imprimir(relatorio,i);
-
+    // Salvar dados no localStorage
+    salvarLocalStorage(relatorio, i);
 }
 
-// Tem que ajustar a impressão
-function imprimir(relatorio, i) {
-    //teste
-    //procurar();
+function salvarLocalStorage(relatorio, i) {
+    if (window.localStorage) {       // Suporte ao localStorage
+        relatorioImpressao = JSON.parse(localStorage.getItem('relatorioImpressao') || '[]');
 
-    let janela = window.open();
+        // Populando de acordo com o parâmetro passado, ou seja, de acordo com o filtro feito na função buscar(relatorio)
+        relatorioImpressao.push({
+            'numeroNF': relatorio.dados[i].numeroNF,
+            'dataEmissao': relatorio.dados[i].dataEmissao,
+            'razaoSocial': relatorio.dados[i].razaoSocial,
+            'chaveAcesso': relatorio.dados[i].chaveAcesso,
+            'valoNF': relatorio.dados[i].valorNF,
+        });
 
-    janela.document.write(relatorio.dados[i].numeroNF, '<br>');
-    janela.document.write(relatorio.dados[i].dataEmissao, '<br>');
-    janela.document.write(relatorio.dados[i].razaoSocial, '<br>');
-    janela.document.write(relatorio.dados[i].chaveAcesso, '<br>');
-    janela.document.write(relatorio.dados[i].valorNF, '<br><br>');
-
+        localStorage.setItem('relatorioImpressao', JSON.stringify(relatorioImpressao));
+    }
 }
+
+function limpaTela() {
+    location.reload();
+}
+
+// Imprimindo 
+function imprimirRelatorio() {
+
+    for (let i = 0; i < localStorage.length; i++) {
+        let key = localStorage.key(i);          // Posição da chave no localStorage
+        let value = localStorage.getItem(key);  // Passando o a chave para ler o valor
+
+        if (key == "relatorioImpressao") {          // Pegando key somente do relatorioImpressao
+            //let str = value;
+            let janela = window.open();
+            janela.document.write("REPX - Relatórios<hr><br><br>");
+            janela.document.write(value);                    // Exibindo localStorage em nova janela
+            janela.document.write("<br><br><hr>")
+            janela.print();            
+
+            limpaTela();
+            localStorage.removeItem('relatorioImpressao');
+            // let janela = window.open();
+            // janela.document.write(str);
+            // formataString(str); // aqui teste
+            localStorage.removeItem('relatorioImpressao');
+        }
+    }    
+}
+
+
+//----------------------------------------------------------------------
+// AQUI PARA BAIXO SÃO OS TESTES DE FORMATAÇÃO DE STRING
+// PARA TESTAR, COMENTA A FUNÇÃO ACIMA imprimirRelatorio()
+
+// // Imprimindo
+// function imprimirRelatorio() {
+
+//     for (let i = 0; i < localStorage.length; i++) {
+//         let key = localStorage.key(i);          // Posição da chave no localStorage
+//         let value = localStorage.getItem(key);  // Passando o a chave para ler o valor
+
+//         if (key == "relatorioImpressao") {      // Pegando key somente do relatorioImpressao
+//             let str = value;
+//             // let janela = window.open();
+//             // janela.document.write("REPX - Relatórios<hr><br><br>");
+//             // janela.document.write(value);                    // Exibindo localStorage em nova janela
+//             // janela.document.write("<br><br><hr>")
+//             // janela.print();            
+
+//             limpaTela();
+//             localStorage.removeItem('relatorioImpressao');
+//             // let janela = window.open();
+//             // janela.document.write(str);
+//             formataString(str);
+//             localStorage.removeItem('relatorioImpressao');
+//         }
+//     }
+// }
+
+// function formataString(str) {
+//     // array para salvar as posições e salvar as alterações.
+//     let newStr = [];            // Esse array será passado para a impressão
+
+//     // Varrendo a string que foi salva em 'str' na função imprimirRelatorio()
+//     for (let i = 0; i < str.length; i++) {
+//         //  document.write(str[i]);      
+//         // Commprando os caracteres para substituir, caso os encontre
+//         if (str[i] == '[' || str[i] == '{' || str[i] == '"' || str[i] == ',' || str[i] == ']' || str[i] == '}') {
+//             if (str[i] == ',') {             // Se a string original tiver ','    
+//                 newStr[i] = str[i];
+//                 newStr[i] = '<br>';          // Então dê um salto de linha e atribua a newStr[i]
+//                 //   document.write(newStr[i]);   // Exibir
+//                 //   imprimir(newStr[i]);
+//             } else {                         // Caso não tenha ','
+//                 newStr[i] = str[i];          // Atribua o caractere atual para a newStr[i]    
+//                 newStr[i] = ' ';             // Substitua o caractere atribuído na newStr[i] para um espaço
+//                 //   document.write(newStr[i]);   // Exibir
+//                 //  imprimir(newStr[i]);
+//             }
+//         } else {                             // Caso o IF não entre, ou seja, caso não encontre os caracteres passados, então só imprima a str[i] com o caractere original
+//             newStr[i] = str[i]              // Atribua o caractere atual para a newStr
+//             //   document.write(newStr[i]);
+//             // imprimir(newStr[i]);
+//         }
+//         if (str[i] == ':') {                  // Após sair do primeiro if/else, vai verificar se tem ':' e adicionar '........'
+//             newStr[i] = '.................:';
+//             //  document.write(newStr[i]);
+//             //imprimir(newStr[i]);
+//         }
+//         // imprimir(newStr);
+//     }
+//     //document.write(newStr);
+
+//     // Aqui vai fazer um laço para percorrer toda string, já alterada
+//     for (let j = 0; j < str.length; j++) {
+//         if (newStr[j] == ',') {
+//             newStr[j] == " ";
+//         }
+//         imprimir(newStr[j]); // Chamando a função, posição a posição. Aqui é o problema
+//     }    
+// }
+
+
+// function imprimir(newStr) { //ERRO
+//     document.write(newStr);
+//     // let janela = window.open();
+//     // janela.document.write("<br><br>");
+//     // janela.print();
+//     // limpaTela();
+//     // localStorage.removeItem('relatorioImpressao');
+//     // let janela = window.open();
+//     // janela.document.write(str);
+// }
+
+// TESTES ATÉ AQUI
+//------------------------------------------------------------------------------
+
