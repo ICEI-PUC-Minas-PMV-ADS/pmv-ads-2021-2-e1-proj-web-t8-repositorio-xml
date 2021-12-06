0faffc8608a5deaa3eb6ee41594a3b43ab810739
CarlosCamuzzi <carloscamuzzi.hs@gmail.com>
2021-12-05 23:56:20 -0300

    Update script-procurar.js

--- script-procurar.js
+++ script-procurar.js
@@ -1,36 +1,49 @@
 // Getando relatório via array
 function procurar() {
-
     let relatorio = {
         dados: [
             {
                 numeroNF: '12',
-                dataEmissao: '01/09/2021',
+                dataEmissao: '27/09/2021',
                 razaoSocial: 'Amazing LTDA',
-                chaveAcesso: '12345678912345678912345678912345678912345678',
-                valorNF: 'R$ 200.00'
+                chaveAcesso: '95687412365478954123698748752134564587963201',
+                valorNF: 'R$ 2050.00'
             },
             {
-                numeroNF: '546',
-                dataEmissao: '01/10/2021',
-                razaoSocial: 'ByeBye LTDA',
-                chaveAcesso: '12345678912345678912345678912345678912345678',
+                numeroNF: '234',
+                dataEmissao: '17/11/2021',
+                razaoSocial: 'Bia Bye LTDA',
+                chaveAcesso: '98700002223335698012475802369582014578902555',
                 valorNF: 'R$ 10000.00'
             },
             {
-                numeroNF: '546',
-                dataEmissao: '01/09/2021',
-                razaoSocial: 'Saudades LTDA',
-                chaveAcesso: '12345678912345678912345678912345678912345678',
+                numeroNF: '2369',
+                dataEmissao: '28/10/2021',
+                razaoSocial: 'Laticínios Big Leite LTDA',
+                chaveAcesso: '14523698745690012000002236669877775012405555',
                 valorNF: 'R$ 2200.00'
             },
             {
                 numeroNF: '546',
                 dataEmissao: '01/10/2021',
-                razaoSocial: 'Abacate',
+                razaoSocial: 'Abacate e Frutas SA',
                 chaveAcesso: '12345678912345678912345678912345678912345678',
                 valorNF: 'R$ 100.00'
-            }
+            },
+            {
+                numeroNF: '2536',
+                dataEmissao: '25/08/2021',
+                razaoSocial: 'Ana Presentes Eireli',
+                chaveAcesso: '12587996548963201254698756250000124445569852',
+                valorNF: 'R$ 550.00'
+            },
+            {
+                numeroNF: '546',
+                dataEmissao: '15/10/2021',
+                razaoSocial: 'Biotech Medicina SA',
+                chaveAcesso: '966521000001255556666987532012548963250125482',
+                valorNF: 'R$ 250.00'
+            }          
         ]
     }
     buscar(relatorio);
@@ -48,19 +61,21 @@ function buscar(relatorio) {
 
     // Iterando o array e verificando se a entrada corresponde com algum elemento
     for (let i = 0; i < relatorio.dados.length; i++) {
-        if (buscaNum == 0 && buscaRazaoSocial == false) {
+        if (buscaNum == 0 && buscaRazaoSocial == false) {       // Se o user não digitou nada, então busca todo array
             //alert("entrou")
             exibirRelatorio(relatorio, i);
         }
-        if (buscaNum) {                 // Por número nota
+        if (buscaNum) {                 // Se digitou numero NF, então compara
             if (buscaNum == relatorio.dados[i].numeroNF) {
                 exibirRelatorio(relatorio, i)
             }
         }
-        else if (buscaRazaoSocial) {        // Por razão social - string
+        else if (buscaRazaoSocial) {        // Se digitou razão social, então compara
             // toUpperCase() para converter tudo para maiúscula, assim o usuário pode digitar em minúscula que não fará diferença
-            let strUpper = relatorio.dados[i].razaoSocial.toUpperCase();
-            if (buscaRazaoSocial.toUpperCase() == strUpper) {
+            let buscaRazaoUpper = buscaRazaoSocial.toUpperCase();
+            let razaoSocialUpper = relatorio.dados[i].razaoSocial.toUpperCase()
+            
+            if (buscaRazaoUpper ==  razaoSocialUpper) {     // Segunda verificação devido a uppercase
                 exibirRelatorio(relatorio, i)
             }
         }
@@ -107,116 +122,61 @@ function limpaTela() {
     location.reload();
 }
 
-// Imprimindo 
+// Imprimindo relatório
 function imprimirRelatorio() {
 
     for (let i = 0; i < localStorage.length; i++) {
         let key = localStorage.key(i);          // Posição da chave no localStorage
         let value = localStorage.getItem(key);  // Passando o a chave para ler o valor
 
-        if (key == "relatorioImpressao") {          // Pegando key somente do relatorioImpressao
-            //let str = value;
-            let janela = window.open();
-            janela.document.write("REPX - Relatórios<hr><br><br>");
-            janela.document.write(value);                    // Exibindo localStorage em nova janela
-            janela.document.write("<br><br><hr>")
-            janela.print();            
-
+        if (key == "relatorioImpressao") {      // Pegando key somente do relatorioImpressao
+            let str = value;
+            formataString(str);
             limpaTela();
             localStorage.removeItem('relatorioImpressao');
-            // let janela = window.open();
-            // janela.document.write(str);
-            // formataString(str); // aqui teste
-            localStorage.removeItem('relatorioImpressao');
         }
-    }    
+    }
 }
 
+function formataString(str) {
+    // Array para salvar as posições e fazer as alterações.
+    let newStr = [];            // Esse array será para salvar as alterações
+    let endStr = [];            // Esse array será para concatenar a string  e passado para impressão
 
-//----------------------------------------------------------------------
-// AQUI PARA BAIXO SÃO OS TESTES DE FORMATAÇÃO DE STRING
-// PARA TESTAR, COMENTA A FUNÇÃO ACIMA imprimirRelatorio()
+    // Varrendo a string que foi salva em 'str' na função imprimirRelatorio()
+    for (let i = 0; i < str.length; i++) {
+        // Comparando os caracteres para substituir, caso os encontre entra nas prox if else
+        if (str[i] == '[' || str[i] == '{' || str[i] == '"' || str[i] == ',' || str[i] == ']' || str[i] == '}') {
+            if (str[i] == ',') {              // Se a string original tiver ','    
+                newStr[i] = str[i];           // Atribua o caractere de str[i] para newStr[i]
+                newStr[i] = '<br>';           // Depois abribua o caractere <br> para saltar uma linha
+            } else {                          // Caso não tenha ',', entra nesse else
+                newStr[i] = str[i];           // Atribua o caractere de str[i] para newStr[i]   
+                newStr[i] = ' ';              // Substitua o caractere atribuído na newStr[i] por um espaço
+            }
+        } else {                               // Caso o IF não entre, ou seja, caso não encontre os caracteres passados no primeiro if
+            newStr[i] = str[i]                 // Atribua o caractere de str[i] para newStr[i]
+        }
+        if (str[i] == ':') {                     // Após sair do primeiro if/else, vai verificar se tem ':' e adicionar '........'
+            newStr[i] = '.................:';    // Se tiver ':' na str[i], então atribua os caracteres '....' para a newStr[i]
+        }
+    }
+    // Aqui vai fazer um laço para percorrer toda string, já alterada
+    for (let j = 0; j < str.length; j++) {
+        if (newStr[j] == ',') {
+            newStr[j] == " ";
+        }
+        endStr += newStr[j];       // Concatenando a string alterada para passar em imprimir();            
+    }
+    janelaImprimir(endStr);     // Chamando impressão
+}
 
-// // Imprimindo
-// function imprimirRelatorio() {
+// Abrindo janela de impressão
+function janelaImprimir(endStr) {                      // Passando a string para impressão
+    window.document.write("REPX - Relatórios<hr><br><br>");
+    window.document.write(endStr, "<br>");                   // Exibindo dados do localStorage formatados em nova janela
+    window.document.write("<br><br><hr>")
+    window.print();
+}
 
-//     for (let i = 0; i < localStorage.length; i++) {
-//         let key = localStorage.key(i);          // Posição da chave no localStorage
-//         let value = localStorage.getItem(key);  // Passando o a chave para ler o valor
-
-//         if (key == "relatorioImpressao") {      // Pegando key somente do relatorioImpressao
-//             let str = value;
-//             // let janela = window.open();
-//             // janela.document.write("REPX - Relatórios<hr><br><br>");
-//             // janela.document.write(value);                    // Exibindo localStorage em nova janela
-//             // janela.document.write("<br><br><hr>")
-//             // janela.print();            
-
-//             limpaTela();
-//             localStorage.removeItem('relatorioImpressao');
-//             // let janela = window.open();
-//             // janela.document.write(str);
-//             formataString(str);
-//             localStorage.removeItem('relatorioImpressao');
-//         }
-//     }
-// }
-
-// function formataString(str) {
-//     // array para salvar as posições e salvar as alterações.
-//     let newStr = [];            // Esse array será passado para a impressão
-
-//     // Varrendo a string que foi salva em 'str' na função imprimirRelatorio()
-//     for (let i = 0; i < str.length; i++) {
-//         //  document.write(str[i]);      
-//         // Commprando os caracteres para substituir, caso os encontre
-//         if (str[i] == '[' || str[i] == '{' || str[i] == '"' || str[i] == ',' || str[i] == ']' || str[i] == '}') {
-//             if (str[i] == ',') {             // Se a string original tiver ','    
-//                 newStr[i] = str[i];
-//                 newStr[i] = '<br>';          // Então dê um salto de linha e atribua a newStr[i]
-//                 //   document.write(newStr[i]);   // Exibir
-//                 //   imprimir(newStr[i]);
-//             } else {                         // Caso não tenha ','
-//                 newStr[i] = str[i];          // Atribua o caractere atual para a newStr[i]    
-//                 newStr[i] = ' ';             // Substitua o caractere atribuído na newStr[i] para um espaço
-//                 //   document.write(newStr[i]);   // Exibir
-//                 //  imprimir(newStr[i]);
-//             }
-//         } else {                             // Caso o IF não entre, ou seja, caso não encontre os caracteres passados, então só imprima a str[i] com o caractere original
-//             newStr[i] = str[i]              // Atribua o caractere atual para a newStr
-//             //   document.write(newStr[i]);
-//             // imprimir(newStr[i]);
-//         }
-//         if (str[i] == ':') {                  // Após sair do primeiro if/else, vai verificar se tem ':' e adicionar '........'
-//             newStr[i] = '.................:';
-//             //  document.write(newStr[i]);
-//             //imprimir(newStr[i]);
-//         }
-//         // imprimir(newStr);
-//     }
-//     //document.write(newStr);
-
-//     // Aqui vai fazer um laço para percorrer toda string, já alterada
-//     for (let j = 0; j < str.length; j++) {
-//         if (newStr[j] == ',') {
-//             newStr[j] == " ";
-//         }
-//         imprimir(newStr[j]); // Chamando a função, posição a posição. Aqui é o problema
-//     }    
-// }
-
-
-// function imprimir(newStr) { //ERRO
-//     document.write(newStr);
-//     // let janela = window.open();
-//     // janela.document.write("<br><br>");
-//     // janela.print();
-//     // limpaTela();
-//     // localStorage.removeItem('relatorioImpressao');
-//     // let janela = window.open();
-//     // janela.document.write(str);
-// }
-
-// TESTES ATÉ AQUI
-//------------------------------------------------------------------------------
 
